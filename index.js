7ca1a1bb8ca59484615774ed7f101c8be1a65d5b
samuelconlon <conlonsp@yahoo.com>
2024-12-11 14:24:49 -0500

    commit tree c1a31e8f83d0a88fc972d513178af08345c5a3bd

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return arr
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int)
+        int--
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function writeCards(name, event) {
+//     const array = [];
+//     for (let i = 0; i < name.length; i++) {
+//         array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+//     }
+//     return array;
+// }
+
+// function countDown(count) {
+//     let i = 10;
+//     while (i >= 0) {
+//         console.log(i--)
+        
+//     }
+// }
